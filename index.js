91f227fa401d628243f526ce4a10ffaf72c7d407
jasnmar <jasnmar@gmail.com>
2024-03-28 15:48:55 -0400

    styling

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 //document.getElementById("count").innerText = 5;
 
-
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count=0;
 
 function increment() {
-    count++
-    const countEl = document.getElementById('count-el');
-    countEl.innerText = count;
+    count++;
+    countEl.textContent = count;
     console.log(count);
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.textContent = count;
+    console.log(count)
 }
